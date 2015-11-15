b6c9fbca99656bff68d85bdd0a8894abb799a2af
Slawomir Borowiec <przodownik@tlen.pl>
2015-11-15 19:59:52 +0100

    thymeleaf conf

--- src/main/java/pl/java/scalatech/config/MvcConfig.java
+++ src/main/java/pl/java/scalatech/config/MvcConfig.java
@@ -1,18 +1,58 @@
 package pl.java.scalatech.config;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+import org.thymeleaf.templateresolver.TemplateResolver;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Configuration
+@Slf4j
 public class MvcConfig extends WebMvcConfigurerAdapter {
-
+    @Autowired
+    private Environment env;
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/home").setViewName("home");
-        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/welcome").setViewName("welcome");
+        registry.addViewController("/").setViewName("welcome");
         registry.addViewController("/hello").setViewName("hello");
         registry.addViewController("/login").setViewName("login");
     }
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/assets/**").addResourceLocations("classpath:/META-INF/resources/webjars/").setCachePeriod(31556926);
+        registry.addResourceHandler("/css/**").addResourceLocations("/css/").setCachePeriod(31556926);
+        registry.addResourceHandler("/images/**").addResourceLocations("/images/").setCachePeriod(31556926);
+        registry.addResourceHandler("/favicon.ico").addResourceLocations("classpath:/static/favicon.ico");
+        registry.addResourceHandler("/js/**").addResourceLocations("/js/").setCachePeriod(31556926);
+
+    }
+
+    @Bean
+    public TemplateResolver templateResolver() {
+        ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver();
+        templateResolver.setPrefix("/templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode("HTML5");
+        List<String> profiles = Arrays.asList(env.getActiveProfiles());
+        if (profiles.contains("dev")) {
+            templateResolver.setCacheable(false);
+            log.info("++++                    templateResolver cache off ... -> dev");
+
+        }
+        templateResolver.setCharacterEncoding("UTF-8");
+        templateResolver.setOrder(2);
+        return templateResolver;
+    }
 }
